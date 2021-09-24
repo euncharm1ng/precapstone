e0aa2b32ebaca3a5b5f91e57c3b46e437b1916a8
uja <21700287@handong.edu>
2021-09-24 19:28:23 +0900

    lcs api for python beta version done

--- everust/lets_connect_six/dummy_six.py
+++ everust/lets_connect_six/dummy_six.py
@@ -2,16 +2,18 @@ from lets_connect_six import *
 import numpy
 import random
 
+dummy_home = -1
+dummy_away = -1
 
 dummy_board = numpy.zeros((19,19))
 
 def create_payload(x1, y1, x2, y2):
-	if x1 > 8: 
+	if x1 > 7: 
 		x1 = x1 + 1
-	if x2 > 8: 
+	if x2 > 7: 
 		x2 = x2 + 1
-	x1 = chr(x1 + 64)
-	x2 = chr(x2 + 64)
+	x1 = chr(x1 + 65)
+	x2 = chr(x2 + 65)
 	y1 = str(y1)
 	y2 = str(y2)
 	if len(y1) == 1:
@@ -24,26 +26,26 @@ def create_payload(x1, y1, x2, y2):
 def make_move():
 	x1 = random.randint(0, 18)
 	y1 = random.randint(0, 18)
-	while dummy_board[x1][y1] != 0:
+	while dummy_board[y1][x1] != 0:
 		x1 = random.randint(0, 18)
 		y1 = random.randint(0, 18)
-	dummy_board[x1][y1] = 9
+	dummy_board[y1][x1] = dummy_home
 		
 	x2 = random.randint(0, 18)
 	y2 = random.randint(0, 18)
-	while dummy_board[x2][y2] != 0:
+	while dummy_board[y2][x2] != 0:
 		x2 = random.randint(0, 18)
 		y2 = random.randint(0, 18)
-	dummy_board[x2][y2] = 9
+	dummy_board[y2][x2] = dummy_home
 	
-	return create_payload(19 - x1, 19 - y1, 19 - x2, 19 - y2)
+	return create_payload(x1, 19 - y1, x2, 19 - y2)
 
 
 def dummy_coor_to_num(coor):
 	x1 = ord(coor[0]) - 65
-	y1 = int(coor[1:3]) - 1
+	y1 = 19 - int(coor[1:3])
 	x2 = ord(coor[4]) - 65
-	y2 = int (coor[5:7]) - 1
+	y2 = 19 - int (coor[5:7])
 
 	if x1 > 8:
 		x1 = x1 - 1
@@ -51,17 +53,27 @@ def dummy_coor_to_num(coor):
 		x2 = x2 - 1
 
 	if x2 < 0 or x2 > 18 or y2 < 0 or y2 > 18 or x1 < 0 or x1 > 18 or y1 < 0 or y1 > 18:
-		print("ERROR: input out of bound in " + home)
+		print("ERROR: input out of bound in " + coor)
 		exit(1)
 
 	return [x1, y1, x2, y2]
 
 
 def main():
+	global dummy_home
+	global dummy_away
+	global dummy_board
 	ip = input("input ip: ")
-	port = input("input port number: ")
-	color = input("input 1 for black, 2 for white: ")
-	red_stones = connect('192.168.137.51', int(port), color)
+	port = int(input("input port number: "))
+	dummy_home = int(input("input 1 for black, 2 for white: "))
+	if dummy_home == 1:
+		dummy_away = 2
+	elif dummy_home == 2:
+		dummy_away = 1
+	else:
+		print("ERROR: input 1 or 2 for color!")
+		exit(1)
+	red_stones = connect('127.0.0.1', port, dummy_home)
 	
 	looper = red_stones.count(':') + 1
 	for i in range(looper):
@@ -70,20 +82,29 @@ def main():
 		x = ord(red_stones[chr_pos]) - 65
 		if x > 8:
 			x = x - 1
-		y = int(red_stones[num_pos:num_pos+2]) - 1 
-		print("red stone: x: " + str(x) + ", y: " + str(y))
-		dummy_board[19 - y][19 - x] = 7
-	print(dummy_board)
+		y = 19 - int(red_stones[num_pos:num_pos+2])
+		dummy_board[y][x] = 3
 
-	if color == 1:
-		dummy_board[9][9] = color
-		draw_and_wait("K10")
+
+	if dummy_home == 1:
+		dummy_board[9][9] = dummy_home
+		away_move = draw_and_wait("K10")
+		[x1, y1, x2, y2] = dummy_coor_to_num(away_move)
+		dummy_board[y1][x1] = dummy_away
+		dummy_board[y2][x2] = dummy_away
+	else:
+		away_move = draw_and_wait("")
+		if away_move != "K10":
+			print("not K10, it is " + away_move)
+			exit(1)
+		dummy_board[9][9] = dummy_away
+
+
 	while 1:
-		away = draw_and_wait(make_move())
-		[x1, y1, x2, y2] = dummy_coor_to_num(away)
-		dummy_board[19 - y1][19 - x1] = 5
-		dummy_board[19 - y2][19 - x2] = 5
-		print(dummy_board)
+		away_move = draw_and_wait(make_move())
+		[x1, y1, x2, y2] = dummy_coor_to_num(away_move)
+		dummy_board[y1][x1] = dummy_away
+		dummy_board[y2][x2] = dummy_away
 	
 if __name__ == "__main__":
 	main()
--- everust/lets_connect_six/lets_connect_six.py
+++ everust/lets_connect_six/lets_connect_six.py
@@ -7,19 +7,24 @@ _socket_to_server.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 _home = -1
 _away = -1
 
-def connect(ip, port, color):
-	global _socket_to_server 	
-	global _home = color
-	if color == 1: 
-		global _away = 2
-	else: 
-		global _away = 1
-	print("trying to connect to server with ip " + ip)
+def connect(ip:str, port:int, color:int):
+	global _home
+	global _away
+	global _lcs_board
+	_home = color
+	if _home == 1: 
+		_away = 2
+	elif _home == 2: 
+		_away = 1
+	else:
+		print("ERROR: input 1 or 2 for color!")
+		exit(1)
+	print("trying to connect to server with ip " + ip + " and port being " + str(port))
 	_socket_to_server.connect((ip, port))
 
 	print("connected to server, waiting for redstones...")
 	size = int.from_bytes(_socket_to_server.recv(4), "little")
-	data = _socket_to_server.recv(size).decode("utf-8").replace(" ", "")
+	data = _socket_to_server.recv(size).decode("utf-8")
 
 	print("this is red stones from server: " + data) 
 	looper = data.count(':') + 1
@@ -29,52 +34,60 @@ def connect(ip, port, color):
 		x = ord(data[chr_pos]) - 65
 		if x > 8:
 			x = x - 1
-		y = int(data[num_pos:num_pos+2]) - 1 
+		y = 19 - int(data[num_pos:num_pos+2])
 		print("red stone: x: " + str(x) + ", y: " + str(y))
-		_lcs_board[19 - y][19 - x] = 3
+		_lcs_board[y][x] = 3
+	print(_lcs_board)
 	return data
 
 
-def draw_and_wait(home):
-	print("draw and wait received: " + str(home))
-	global _socket_to_server
-	global color
-	home = home.replace(" ", "").upper()
-	if home == "K10" or home == "":
+def draw_and_wait(user_move:str):
+	global _lcs_board
+	print("draw and wait received: " + user_move)
+	user_move = user_move.replace(" ", "").upper()
+	if user_move == "K10":
+		_lcs_board[9][9] = _home
+	elif user_move == "":
 		pass
-	elif home[0] == 'I' or home[4] == 'I':
-		print("ERROR: \'I\' included in input " + home)
+	elif user_move[0] == 'I' or user_move[4] == 'I':
+		print("ERROR: \'I\' included in input " + user_move)
 		exit(1)
 	else: 
-		[x1, y1, x2, y2] = _coor_to_num(home)
+		[x1, y1, x2, y2] = _coor_to_num(user_move)
 		if _lcs_board[y1][x1] != 0 or _lcs_board[y2][x2] != 0:
-			print(str(home) + " is not empty!")
+			print(str(user_move) + " is not empty!")
 			exit(1)
 		else:
-			_lcs_board[y1][x1] = color
-			_lce_board[y2][x2] = color
+			_lcs_board[y1][x1] = _home
+			_lcs_board[y2][x2] = _home
 		
-	if len(home):
-		_socket_to_server.sendall((len(home)).to_bytes(4, byteorder='little') + str.encode(home))
-	print(home + " is sent, waiting for server")
+	if len(user_move):
+		_socket_to_server.sendall((len(user_move)).to_bytes(4, byteorder='little') + str.encode(user_move))
+		print(user_move + " is sent, waiting for server")
+
+	print("waiting for server...")
 	size = int.from_bytes(_socket_to_server.recv(4), "little")
-	away = _socket_to_server.recv(size).decode("utf-8").replace(" ", "")
-	print("this is away from server: " + str(away))
-	[x1, y1, x2, y2] = _coor_to_num(away)
-	_lcs_board[y1][x1] = _away
-	_lcs_board[y2][x2] = _away
-	return away
+	away_move = _socket_to_server.recv(size).decode("utf-8")
+	print("this is away from server: " + away_move)
+	if away_move == "K10":
+		_lcs_board[9][9] = _away
+	else: 
+		[x1, y1, x2, y2] = _coor_to_num(away_move)
+		_lcs_board[y1][x1] = _away
+		_lcs_board[y2][x2] = _away
+	print(_lcs_board)
+	return away_move
 
 
-def get_lcs_board(ask):
+def get_lcs_board(ask:str):
 	pass
 
 
 def _coor_to_num(coor):
 	x1 = ord(coor[0]) - 65
-	y1 = int(coor[1:3]) - 1
+	y1 = 19 - int(coor[1:3])
 	x2 = ord(coor[4]) - 65
-	y2 = int (coor[5:7]) - 1
+	y2 = 19 - int (coor[5:7])
 
 	if x1 > 8:
 		x1 = x1 - 1
@@ -82,26 +95,14 @@ def _coor_to_num(coor):
 		x2 = x2 - 1
 
 	if x2 < 0 or x2 > 18 or y2 < 0 or y2 > 18 or x1 < 0 or x1 > 18 or y1 < 0 or y1 > 18:
-		print("ERROR: input out of bound in " + home)
+		print("ERROR: input out of bound in " + coor)
 		exit(1)
 
-	return [19 - x1, 19 - y1, 19 - x2, 19 - y2]
+	return [x1, y1, x2, y2]
 
-
-def _num_to_coor(x, y):
-	if x > 8: 
-		x = x + 1
-	x = chr(x + 64)
-	y = str(y)
-	if len(y) == 1:
-		y = "0" + y
-	return "" + x + y 
-	
 
 def main():
-	red_stones = connect('192.168.137.51', 8080, 1)
-	oppo_move = draw_and_wait("A09:S16")
-	print(oppo_move)
+	pass
 
 
 if __name__ == "__main__":
