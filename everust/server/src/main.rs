2706e4960b1cad95f5761345541d0b5a270e8bfc
uja <21700287@handong.edu>
2021-11-14 21:57:29 +0900

    board_operation updated

--- everust/server/src/main.rs
+++ everust/server/src/main.rs
@@ -1,7 +1,8 @@
-use server::board_operation::*;
+#![allow(unused)]
+mod board_operation;
+use board_operation::*;
+
 fn main() {
-	let mut myboard = Board::new("A01:B01");
-	println!("check and forward {}", myboard.check_and_forward("K10", BLACK));
-	println!("check and forward {}", myboard.check_and_forward("K11:K12", WHITE));
+	let mut myboard = Board::new("");
 	myboard.print_board();
 }
